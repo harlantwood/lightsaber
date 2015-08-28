96e40da5703efeb8ee2e44a58334fb857bdcedda
Harlan T Wood <code+harlan@harlantwood.net>
2015-08-28 15:04:00 -0700

    remove snake case verison of lodash

--- index.js
+++ index.js
@@ -2,9 +2,7 @@ var _ = require('lodash');
 
 module.exports = _.merge(
   require('./lib/hash'),
-  require('./lib/lodash'),
   require('./lib/log'),
-  require('./lib/object'),
   require('./lib/space'),
   require('./lib/string'),
   require('./lib/type'),
--- src/lodash.coffee
+++
@@ -1,5 +0,0 @@
-{ snake_case_keys } = require './object'
-
-lodash_snake_case = snake_case_keys require "lodash"
-
-module.exports = { lodash_snake_case }
